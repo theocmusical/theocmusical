4965290d1f977fdbadf4eda1b88132f98bffd18b
Stewart Yu <stewartyu@gmail.com>
2016-02-20 22:12:44 -0800

    add parallax

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+!(function() {
+    var fixHeroHeight = function() {
+        var heroContainer = document.querySelector('.hero');
+        var heroImage = document.querySelector('.hero__image');
+        var setHeroHeight = function() {
+            heroContainer.style.height = heroImage.clientHeight + "px";
+        };
+
+        setHeroHeight();
+
+        window.addEventListener('resize', function() {
+            window.requestAnimationFrame(setHeroHeight);
+        }, true);
+    };
+
+    var parallaxHero = function() {
+        var heroImage = document.querySelector('.hero__image');
+        window.addEventListener('scroll', function() {
+            window.requestAnimationFrame(function() {
+                var y = window.scrollY;
+                heroImage.style = 'transform: translate3d(0px, ' + (y / 2) + 'px, 0)';
+            });
+        });
+    };
+
+    fixHeroHeight();
+    parallaxHero();
+})();
